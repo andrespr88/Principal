cb5bc7c9d0a5a4ebf5a9bcb301ebaab14277302d
andrespr88 <andrescruz_pr@hotmail.com>
2018-12-28 10:39:13 -0300

    asd

--- programacion/programadorWeb-base-master/Clase 14/Profe/Desafío/js/index.js
+++ programacion/programadorWeb-base-master/Clase 14/Profe/Desafío/js/index.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
-  $('button').on('click', removeElementWithAnimation)
+$(document).ready(function() {
+	$('button').on('click', removeElementWithAnimation);
 
-  /**
+	/**
  * removeElementWithAnimation es una función que recibe un evento
  * y elimina el nodo con una animación
  * @param {*} event
  */
-  function removeElementWithAnimation (event) {
-    $(this).parent().parent().hide(2000, function () {
-      $(this).remove()
-    })
-  }
-})
+	function removeElementWithAnimation(event) {
+		$(this).parent().parent().hide(2000, function() {
+			$(this).remove();
+		});
+	}
+});
